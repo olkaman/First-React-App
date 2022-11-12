8c2f4684b90aebdbd23853512063c4a9ec9b5169
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-11-12 14:54:26 +0100

    Add first components

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,13 @@
+import styles from './TextInput.module.scss';
+
+const TextInput = (props) => {
+  return (
+    <input
+      className={styles.input}
+      type='text'
+      placeholder={props.placeholder}
+    />
+  );
+};
+
+export default TextInput;
