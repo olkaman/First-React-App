f807c14bab8535710ab98360c26b1ed7151abbbf
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-11-27 15:05:20 +0100

    Add searchform functionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,8 +4,12 @@ import CardForm from '../CardForm/CardForm';
 import { useSelector } from 'react-redux';
 
 const Column = (props) => {
+  const phrase = useSelector((state) => state.searchPhrase);
   const cards = useSelector((state) =>
-    state.cards.filter((card) => card.columnId === props.id)
+    state.cards.filter(
+      (card) =>
+        card.columnId === props.id && card.title.toLowerCase().includes(phrase)
+    )
   );
 
   return (
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,25 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_SEARCHED_VALUE', payload: searchValue });
+    setSearchValue('');
+  };
+
   return (
-    <form className={styles.searchForm}>
-      <TextInput placeholder='Search...' />
+    <form className={styles.searchForm} onSubmit={handleSearch}>
+      <TextInput
+        placeholder='Search...'
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
       <Button>
         <span className='fa fa-search' />
       </Button>
