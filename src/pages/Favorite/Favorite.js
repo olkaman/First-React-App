078b4fc82e06d02bb9754fe57583b5d25f212bd9
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-12-09 22:01:49 +0100

    Remove favorite cards from favorite list

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -7,12 +7,20 @@ import Card from '../../components/Card/Card';
 const Favorite = () => {
   const cards = useSelector((state) => getFavoriteCards(state));
 
+  if (cards.length === 0) {
+    return <h2 className={styles.noFavoriteCards}>No favorite cards</h2>;
+  }
   return (
     <div className={styles.wrapper}>
       <PageTitle>Favorite</PageTitle>
       <article className={styles.column}>
         {cards.map((card) => (
-          <Card key={card.id} title={card.title} favorite={card.isFavorite} />
+          <Card
+            key={card.id}
+            title={card.title}
+            favorite={card.isFavorite}
+            id={card.id}
+          />
         ))}
       </article>
     </div>
